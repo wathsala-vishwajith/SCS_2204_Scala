5b4e14dc5dec3c0692ee941a4168fde51efb4f93
wathsala-vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-06-11 12:09:13 +0530

    Create file1.scala

---
+++ test/file1.scala
@@ -0,0 +1,7 @@
+val a=scala.io.StdIn.readInt()
+val b=scala.io.StdIn.readInt()
+
+println("The value of a+b is " + (a+b))
+println("The value of a-b is " + (a-b))
+println("The value of a*b is " + (a*b))
+println("The value of a/b is " + (a/b))
