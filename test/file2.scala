f1bed1f3aecb525d9e4304c9dd3423a6e0f49e8a
wathsala-vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-06-17 21:12:49 +0530

    yield

--- test/file2.scala
+++ test/file2.scala
@@ -30,3 +30,14 @@ val a2 = a1.map(_*3)
 a2.foreach(println)
 
 val ary = new Array[Int](5) //new Array in size of 5
+
+var a = 0; 
+val numList = List(1,2,3,4,5,6,7,8,9,10); 
+// for loop execution with a yield 
+var retVal = for{ a <- numList 
+if a != 3; if a < 8 
+}yield a //adds the a to the retVal
+// Now print returned values using another loop. 
+for( a <- retVal){ 
+println( "Value of a: " + a ); 
+} 
