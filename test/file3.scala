fb1f989453940010b62f08fd3f0afcab8500f571
wathsala-vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-06-18 11:50:32 +0530

    Create file3.scala

---
+++ test/file3.scala
@@ -0,0 +1,9 @@
+object Demo{
+    def main(args: Array[String]){
+        println(addInt(4,5))
+    }   
+    //def addInt(a:Int, b:Int):Int={
+    //    a+b;
+    //}
+    def addInt(a:Int, b:Int) =a+b;
+}
