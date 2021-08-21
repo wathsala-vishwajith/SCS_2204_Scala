80486d1a518f8bb7a8f97dd387c453cff9308c8b
wathsala-vishwajith <wathsalavishwajith@github.com>
2021-08-21 14:16:41 +0530

    q1

--- Assignment - Functions and Data/q1.scala
+++ Assignment - Functions and Data/q1.scala
@@ -1,1 +1,13 @@
 
+object q1 extends App{
+  val r1 = new Rational(1,5)
+  r1.neg.print
+}
+
+class Rational(x:Int,y:Int){
+  private def numer = x
+  private def denom = y
+
+  def neg = new Rational(-1 * this.numer, this.denom)
+  def print = println(this.numer+"/"+this.denom)
+}
